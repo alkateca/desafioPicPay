41d59907e8dccb71193cd9647e29919d7be0f5af
alkateca <chesini.mendes@gmail.com>
2025-12-26 23:18:05 -0300

    first commit

---
+++ src/test/java/com/alkateca/picpaysimplicado/PicpaysimplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkateca.picpaysimplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplicadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
